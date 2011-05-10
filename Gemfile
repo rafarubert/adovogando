2d210aee852185bbb8b66c903e058735021b30d7
Rafael Cruz Rubert <rafarubert@yahoo.com.br>
2011-05-09 21:35:29 -0300

    First commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7.rc2'
+
+gem 'sqlite3'
+
+gem 'devise'
+gem "oa-oauth", :require => "omniauth/oauth"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdvogandoMe::Application.config.secret_token = '564e5cf5a653c0b31a331b5d2a1f3663e54d147c392de88071e70338cdee750b6704972f8fddf167f9f10635b1ac49e1ccb38450abc9c34abfacc1274b6d03da'
